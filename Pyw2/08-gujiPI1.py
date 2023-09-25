c862e3047d608df76b9efa30d41d90b471ee1879
unknown <10225501457@stu.ecnu.edu.cn>
2023-09-25 19:53:08 +0800

    add a new wenjianjia

--- Pyw2/08-gujiPI1.py
+++ Pyw2/08-gujiPI1.py
@@ -40,3 +40,20 @@ def Est(precision, numtrails):
 
 
 Est(0.01, 100)
+
+# 布冯-拉普拉斯方法
+# Est = 3.13844 , Stdev = 0.05425 , Numneedles = 1000 ,
+# Est = 3.1404 , Stdev = 0.03335 , Numneedles = 2000 ,
+# Est = 3.13965 , Stdev = 0.02808 , Numneedles = 4000 ,
+# Est = 3.14439 , Stdev = 0.01684 , Numneedles = 8000 ,
+# Est = 3.1420125 , Stdev = 0.01235 , Numneedles = 16000 ,
+# Est = 3.14167625 , Stdev = 0.00882 , Numneedles = 32000 ,
+# Est = 3.141705625 , Stdev = 0.00621 , Numneedles = 64000 ,
+# Est = 3.141698125 , Stdev = 0.00483 , Numneedles = 128000
+# 无穷级数
+# 3.1413926536
+# 梅钦公式
+# 3.1415926536
+
+# 由以上三种方法所得的Pi的估计值对比，梅钦公式的效率最高，精度也最高。其次是无穷级数。
+# 蒙特卡罗方法中，随着投针数增加，会增加结果的精确度，但是效果不明显。
