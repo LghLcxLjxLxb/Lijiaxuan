ace12f90e479bf14d3cdc252b3fdee123c767b04
unknown <10225501457@stu.ecnu.edu.cn>
2024-01-15 20:03:03 +0800

    delete Pyw3

--- Pyw3/03-zhengtest.py
+++
@@ -1,7 +0,0 @@
-# 简单验证身份证号是否合法
-import re
-card = input('请输入待验证的身份证号：')
-pattern = re.compile(
-    r'^[1-9]\d{5}(18|19|20)\d{2}(0[1-9]|1[0-2])(([0-2][1-9])|10|20|(3[0-1]))\d{3}[\dX]$')
-result = pattern.search(card)
-print(result)
--- Pyw3/06-fenzhi.py
+++
@@ -1,9 +0,0 @@
-store = int(input('请输入一个你的分数：'))
-if store<60 :
-  print('不及格')
-elif store>=60 and store<=74:
-  print('及格')
-elif store>=75 and store<=89:
-  print('良好')
-elif store>=90:
-  print('优秀')
